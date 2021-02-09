da6efa88348c8c93f54ff831ccf948593731ed8d
Hillel Arnold <helrond@hotmail.com>
2021-02-09 14:03:08 -0500

    better print and caching

--- transformer/cron.py
+++ transformer/cron.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django_cron import CronJobBase, Schedule
 
 from .mappings import has_online_asset
@@ -10,9 +12,11 @@ class CheckMissingOnlineAssets(CronJobBase):
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
 
     def do(self):
+        print("Checking for recently added assets at {}".format(datetime.now()))
         for object in DataObject.objects.filter(object_type__in=["collection", "object"], data__online=False):
             if has_online_asset(object.es_id):
                 object.data["online"] = True
                 object.indexed = False
                 object.save()
                 print("Online assets discovered for {}".format(object.es_id))
+        print("Finished checking for recently added assets at {}\n".format(datetime.now()))
